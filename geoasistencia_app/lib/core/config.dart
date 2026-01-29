775fedff0e4a6c4787670729b9335cf10f205093
luisf618 <147664689+luisf618@users.noreply.github.com>
2026-01-29 10:26:07 -0500

    Script

--- geoasistencia_app/lib/core/config.dart
+++ geoasistencia_app/lib/core/config.dart
@@ -1,5 +1,5 @@
 class AppConfig {
   /// Para emulador Android: 10.0.2.2 apunta a tu PC (localhost).
   /// Para dispositivo físico en la misma red: usa la IP de tu PC (ej: http://192.168.1.10:8000)
-  static const String baseUrl = 'http://10.0.2.2:8000';
+  static const String baseUrl = 'http://172.17.98.19:8000';
 }
